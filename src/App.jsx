6c3b1e1759d4fa77359f6a32534053fee7a3a83e
DiogomCunha <ddmoraes.ddm@gmail.com>
2023-02-14 22:43:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App(){
+
+  return(
+    <div>
+      <h1>Olá Mundo!</h1>
+      <p>Projeto Criado utilizando React</p>
+      <p>Abaixo o GitHub com o repositorio.</p>
+      <a href="https://github.com/DiogomCunha" target="_blank"><img src="./img/icons8-github-30.png" alt="" /></a>
+      
+    </div>
+  )
+
+}
+
+export default App;
+
+//Diogo de Moraes Cunha
+//Turma 1 6 Bimestre Front III
